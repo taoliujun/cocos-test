3892e81d50923a18403b491377c948d20cb47c45
taoliujun <taoliujun@qq.com>
2024-03-27 21:21:04 +0800

    fix: update

--- assets/scripts/boxController.ts
+++ assets/scripts/boxController.ts
@@ -1,4 +1,17 @@
-import { _decorator, Color, Component, instantiate, Node, Prefab, Sprite, UITransform, Vec2, Vec3 } from 'cc';
+import {
+    _decorator,
+    Color,
+    Component,
+    Graphics,
+    instantiate,
+    Node,
+    Prefab,
+    Sprite,
+    UITransform,
+    Vec2,
+    Vec3,
+    Widget,
+} from 'cc';
 import { getRandomColor } from './utils/color';
 
 const { ccclass, property } = _decorator;
@@ -13,31 +26,41 @@ export class boxController extends Component {
         const yCount = 10;
 
         const item = instantiate(this.lotteryPrefab);
-        item.setPosition(new Vec3(200 * (index - 2), 300, 0));
+        item.addComponent(Widget);
+
+        const widget = item.getComponent(Widget);
+        widget.isAlignLeft = true;
+        widget.isAlignTop = true;
+        widget.left = 200 * (index - 1);
+        widget.top = 0;
 
         const ui = item.getComponent(UITransform);
-        ui.setAnchorPoint(new Vec2(0.5, 1));
         ui.width = 200;
         ui.height = 200 * yCount;
 
         const sprite = item.getComponent(Sprite);
-        sprite.color = new Color(0, 0, 0);
+        sprite.color = getRandomColor();
 
-        console.log(`==========scroll${index}`, item.position);
+        // console.log(`==========scroll${index}`, item.position);
 
         for (let i = 1; i <= yCount; i += 1) {
             const citem = instantiate(this.lotteryPrefab);
-            citem.setPosition(new Vec3(0, (yCount / 2 - i) * 200, 0));
-            console.log(`==item${i}`, citem.position);
+            citem.addComponent(Widget);
+
+            const cwidget = citem.getComponent(Widget);
+            cwidget.isAlignLeft = true;
+            cwidget.isAlignTop = true;
+            cwidget.left = 0;
+            cwidget.top = 200 * (i - 1);
 
             const cui = citem.getComponent(UITransform);
-            // ui.setAnchorPoint(new Vec2(0.5, 1));
             cui.width = 200;
             cui.height = 200;
 
             const csprite = citem.getComponent(Sprite);
-
             csprite.color = getRandomColor();
+
+            // console.log(`==item${i}`, citem.position);
 
             item.addChild(citem);
         }
@@ -46,10 +69,19 @@ export class boxController extends Component {
     }
 
     start() {
+        const g = this.node.parent.getChildByName('tmpNode').getComponent(Graphics);
+        g.moveTo(-300, 300);
+        g.lineTo(300, 300);
+        g.lineTo(300, -300);
+        g.lineTo(-300, -300);
+        g.close();
+        g.stroke();
+
         this.node.addChild(this.generateScrollItem(1));
         this.node.addChild(this.generateScrollItem(2));
         this.node.addChild(this.generateScrollItem(3));
-        console.log('==item1', this.node.getChildByName('scrollItem_1'));
+        console.log('==item1', this.node.children[0].children[1].getComponent(Widget));
+
         // console.log('==node1', this.node.getChildByName('node1'));
         // this.makeBoxes();
     }
